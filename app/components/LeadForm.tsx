aa4347d1cb0bbcb6f0992f9ded43cac0185121cf
tartaromaxam@hotmail.com <tartaromaxam@hotmail.com>
2026-06-07 16:10:04 -0300

    chore: atualiza URLs do webhook do Make para a v2 no site e calculadora

--- app/components/LeadForm.tsx
+++ app/components/LeadForm.tsx
@@ -33,7 +33,7 @@ export default function LeadForm(): React.JSX.Element {
           body: JSON.stringify(formData),
           signal: controller.signal
         }),
-        fetch("https://hook.us2.make.com/uenwaqqn6cjyrx754med5l5hcarhby46", {
+        fetch("https://hook.us2.make.com/5177ub4bdfmkujgdw811913c42mf81b3", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(formData),
